a9d7e8df25adf52029eab0bf59c22618f8ea3e85
Abhishek Prajapati <46470516+blackviking27@users.noreply.github.com>
2021-07-24 11:21:01 +0530

    Update Thumbnail.js

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -15,6 +15,7 @@ const Thumbnail = forwardRef(({result}, ref) => {
                 }
                 height={1080}
                 width={1920}
+                alt="Poster"
             />
             <div className='p-2' >
                 <p className="truncate max-w-md" >{result.overview}</p>
